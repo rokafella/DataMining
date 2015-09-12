a5ff1e22febd8deb1686952d39c0ea83965e9dfe
Nandu Khobare <nandu.khobare@gmail.com>
2015-09-12 01:16:05 -0400

    Calculated word frequencies

--- Preprocessor/reader.py
+++ Preprocessor/reader.py
@@ -7,10 +7,12 @@ from nltk.corpus import stopwords
 allfiles = glob.glob("../DataSet/*.sgm")
 tags = ['topics', 'places', 'title', 'dateline', 'body']
 stop = stopwords.words('english')
+word_frequency = {}
 
 
 def remove_tags(text):
     return ' '.join(eT.fromstring(text).itertext())
+
 
 for datafile in allfiles:
     f = open(datafile, 'r')
@@ -20,11 +22,17 @@ for datafile in allfiles:
         plain_string = remove_tags(str(collections[0]).replace('', ''))
         final_string = plain_string.translate(string.maketrans('', ''), string.punctuation).lower()
         filtered_words = [word for word in final_string.split() if word not in stop]
+
         print filtered_words
-# f = open('../DataSet/reut2-000.sgm', 'r')
-# data = f.read()
-# soup = BeautifulSoup(data, 'html.parser')
-# bodies = soup.findAll('body')
-# for body in bodies:
-#     words = str(body)[6:-9].split()
-#     print words[0]
+
+        for filtered_word in filtered_words:
+            if filtered_word in word_frequency.keys():
+                word_frequency[filtered_word] += 1
+            else:
+                word_frequency[filtered_word] = 1
+
+print("Word frequencies are : ")
+print(word_frequency)
+word_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
+print("Word frequencies in descending order are : ")
+print(word_frequency)
