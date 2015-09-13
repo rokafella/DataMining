e206cdc11155d6967b9d5352027149c5467edc10
Rohit Kapoor <rohit6301@gmail.com>
2015-09-13 14:42:14 -0400

    Reading all tags

--- Preprocessor/reader.py
+++ Preprocessor/reader.py
@@ -7,32 +7,26 @@ from nltk.corpus import stopwords
 allfiles = glob.glob("../DataSet/*.sgm")
 tags = ['topics', 'places', 'title', 'dateline', 'body']
 stop = stopwords.words('english')
-word_frequency = {}
 
 
 def remove_tags(text):
     return ' '.join(eT.fromstring(text).itertext())
 
+total = 0
 
 for datafile in allfiles:
     f = open(datafile, 'r')
     soup = BeautifulSoup(f.read(), 'html.parser')
+
     for tag in tags:
         collections = soup.findAll(tag)
-        plain_string = remove_tags(str(collections[0]).replace('', ''))
-        final_string = plain_string.translate(string.maketrans('', ''), string.punctuation).lower()
-        filtered_words = [word for word in final_string.split() if word not in stop]
+        for collection in collections:
+            plain_string = remove_tags(str(collection).replace('', ''))
+            final_string = plain_string.translate(string.maketrans('', ''), string.punctuation).lower()
+            filtered_words = [word for word in final_string.split() if word not in stop]
 
-        print filtered_words
+            total += len(filtered_words)
 
-        for filtered_word in filtered_words:
-            if filtered_word in word_frequency.keys():
-                word_frequency[filtered_word] += 1
-            else:
-                word_frequency[filtered_word] = 1
+    print datafile + " done"
 
-print("Word frequencies are : ")
-print(word_frequency)
-word_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
-print("Word frequencies in descending order are : ")
-print(word_frequency)
+print total
