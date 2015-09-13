1a61d377969ea2b2ddaebc06e0888dcb5107a23c
Rohit Kapoor <rohit6301@gmail.com>
2015-09-13 15:56:16 -0400

    Counting frequency of all words

--- Preprocessor/reader.py
+++ Preprocessor/reader.py
@@ -1,32 +1,34 @@
 from bs4 import BeautifulSoup
 import glob
-import xml.etree.ElementTree as eT
 import string
+import nltk
+from collections import Counter
 from nltk.corpus import stopwords
 
 allfiles = glob.glob("../DataSet/*.sgm")
 tags = ['topics', 'places', 'title', 'dateline', 'body']
 stop = stopwords.words('english')
 
-
-def remove_tags(text):
-    return ' '.join(eT.fromstring(text).itertext())
-
 total = 0
+
+words = []
 
 for datafile in allfiles:
     f = open(datafile, 'r')
-    soup = BeautifulSoup(f.read(), 'html.parser')
+    soup = BeautifulSoup(f.read(), "html.parser")
 
-    for tag in tags:
-        collections = soup.findAll(tag)
-        for collection in collections:
-            plain_string = remove_tags(str(collection).replace('', ''))
-            final_string = plain_string.translate(string.maketrans('', ''), string.punctuation).lower()
-            filtered_words = [word for word in final_string.split() if word not in stop]
-
-            total += len(filtered_words)
+    for article in soup.findAll('reuters'):
+        for tag in tags:
+            text = article.find(tag)
+            if text:
+                text = text.text.encode("ascii", "ignore")
+                final_string = text.translate(None, string.punctuation).lower()
+                filtered_words = [word for word in final_string.split() if word not in stop]
+                words += filtered_words
+                total += len(filtered_words)
 
     print datafile + " done"
 
-print total
+print str(total) + " words"
+tokens = nltk.word_tokenize(' '.join(words))
+print Counter(tokens).most_common(10)
