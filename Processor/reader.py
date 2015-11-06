94316f7149fcaed927594a150d35584e44d9f9f0
Rohit Kapoor <rohit6301@gmail.com>
2015-11-05 23:39:02 -0500

    Clustering complete

--- Processor/reader.py
+++ Processor/reader.py
@@ -5,6 +5,7 @@ import time
 import math
 import csv
 import Orange
+import numpy
 from newcollections import Counter
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -95,13 +96,14 @@ feature_vector.append(attribute_type)
 
 feature_type = ['c']
 feature_vector.append(feature_type)
+topic_seq = []
 
 # Creating the feature vectors by iterating over feature words
 for i in tf.keys():
     topics = topic_dict[i]
     for topic in topics:
         row = [topic]
-
+        topic_seq.append(topic)
         for word in feature_words:
             if word in tf[i]:
                 row.append(1)
@@ -119,15 +121,98 @@ file_name = '../Output/FeatureVector.tab'
 # Loading the feature vector in Orange
 data = Orange.data.Table(file_name)
 
-print "Starting clustering"
-
 
 def callback(km):
     print "Iteration: %d, changes: %d, score: %.4f" % (km.iteration, km.nchanges, km.score)
 
-km = Orange.clustering.kmeans.Clustering(data, 3, minscorechange=0, inner_callback=callback)
 
-clusters = Counter(km.clusters)
+def entropy(clusters):
+        container = {}
 
-print clusters
-print Orange.clustering.kmeans.score_fast_silhouette(km)
+        for t, cluster in clusters:
+            if cluster in container:
+                container[cluster].append(t)
+            else:
+                container[cluster] = [t]
+
+        entropy_final = 0.0
+
+        for id in container.keys():
+            topic_count = Counter(container[id])
+            ent = 0.0
+            for t in topic_count.keys():
+                ent += (float(topic_count[t]) / clusters_count[id]) * math.log((float(clusters_count[id]) / topic_count[t]), 2)
+            entropy_final += (float(clusters_count[id]) / len(topic_seq)) * ent
+        return entropy_final
+
+choice = input("Please select 1->Kmeans and 2->Hierarchical: ")
+
+distance = input("Please select a distance metric 1->Euclidean and 2->Manhattan: ")
+
+if distance == 1:
+    distance = Orange.distance.Euclidean
+else:
+    distance = Orange.distance.Manhattan
+
+clusters_count = {}
+
+if choice == 1:
+
+    print "Starting Kmeans clustering"
+
+    start = time.clock()
+
+    km = Orange.clustering.kmeans.Clustering(data, 77, minscorechange=0, inner_callback=callback, distance=distance)
+
+    end = time.clock()
+
+    print "Clustering time: " + str(end - start)
+
+    clusters_count = Counter(km.clusters)
+
+    print 'Silhouette: ' + str(Orange.clustering.kmeans.score_fast_silhouette(km))
+    print 'Entropy: ' + str(entropy(zip(topic_seq, km.clusters)))
+    print 'Variance: ' + str(numpy.var(clusters_count.values()))
+
+else:
+
+    linkage = input("Please select a linkage 1->Single, 2->Average, 3->Complete: ")
+
+    if linkage == 1:
+        linkage = Orange.clustering.hierarchical.SINGLE
+    elif linkage == 2:
+        linkage = Orange.clustering.hierarchical.AVERAGE
+    else:
+        linkage = Orange.clustering.hierarchical.COMPLETE
+
+    print "Starting Hierarchical clustering"
+
+    start = time.clock()
+
+    matrix = Orange.distance.distance_matrix(data, distance)
+
+    clustering = Orange.clustering.hierarchical.HierarchicalClustering()
+    clustering.linkage = linkage
+
+    root = clustering(matrix)
+
+    root.mapping.objects = data
+
+    end = time.clock()
+
+    print "Clustering time: " + str(end - start)
+
+    topmost = sorted(Orange.clustering.hierarchical.top_clusters(root, 77), key=len)
+
+    ls_topic = []
+
+    for n, cluster in enumerate(topmost):
+        print str(n) + '--->' + str(len(cluster))
+        clusters_count[n] = len(cluster)
+        for inst in cluster:
+            ls_topic.append((str(inst[-1]), n))
+
+    print ls_topic
+    print clusters_count
+    print 'Entropy: ' + str(entropy(ls_topic))
+    print 'Variance: ' + str(numpy.var(clusters_count.values()))
